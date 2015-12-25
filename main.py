7d716c044925ad846879ec950274185fa38430e0
Surya Teja Cheedella <shine123surya@gmail.com>
2015-12-25 20:21:47 +0530

    added parsing the fetched file using bs4

--- main.py
+++ main.py
@@ -1,24 +1,82 @@
 import os
-import requests, bs4
+import requests
+from bs4 import BeautifulSoup
+import time, datetime
 
 def getPosts(url):
     out_file= open("posts.html", "w")
 
     r= requests.get(url)
-    print(r.text)
+    #print(r.text)
     out_file.write(r.text)
+    out_file.close()
 
-    #print(r.response_code)
 
 
-def makeDayoneEntry():
-    text= '''<p>akdjfaj adjfk klajdf alsjsf </p>'''
-    outText= "echo '"+ text+ "' | dayone new"
+def parsePosts(file_name, interest_date):
+    posts_file= open(file_name, "r")
+    posts_data= posts_file.read()
+    posts_file.close()
+
+    entries= []
+    flag= True
+
+    while flag and '<a href="/SnShines/status' in posts_data:
+        entry_text= ""
+        posts_data= posts_data[posts_data.index('<a href="/SnShines/status'):]
+        soup= BeautifulSoup(posts_data, "html.parser")
+
+        unix_time= (soup.a.span["data-time"])
+        post_date= str(datetime.datetime.fromtimestamp(int(unix_time)))
+
+        #print(post_date[:10], interest_date)
+
+        if (post_date[:10])== interest_date:
+            entry_text+= post_date
+            entry_text+= "\n\n"
+            entry_text+= soup.a["href"]
+            entry_text+= "\n\n"
+            entry_text+= str(soup.p)
+            entry_text+= "\n\n\n"
+
+            entries.append(entry_text)
+        else:
+            pass
+            #flag= False
+
+        # print(soup.a)
+        # print(soup.p)
+        # print("\n")
+
+
+        posts_data= posts_data[1:]
+
+    return entries
+
+
+
+def makeDayoneEntry(entries):
+    entries= entries[::-1]
+    #print(entries)
+
+    entry_text= "".join(entries)
+
+    print(entry_text)
+
+    outText= "echo '"+ entry_text+ "' | dayone new"
     os.system(outText)
+
 
 
 if __name__== "__main__":
     URL= "https://twitter.com/SnShines"
+    interest_date= (time.strftime("%Y-%m-%d"))
+    interest_date= "2015-12-21"
+
     #getPosts(URL)
-    #parsePosts()
-    #makeDayoneEntry()
+
+    entries= parsePosts("posts.html", interest_date)
+
+    makeDayoneEntry(entries)
+
+    # '<a href="/SnShines/status'
